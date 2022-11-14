86ce8eb6eb603decc9575ad0554ef0cd197c2615
Luca Sorrento <Luca Sorrento@192.168.1.59>
2022-11-14 18:09:24 +0100

    Commit CorsistaBC

---
+++ GestioneCorsi/src/main/java/com/bari/gestionecorsi/businesscomponent/CorsistaBC.java
@@ -0,0 +1,37 @@
+package com.bari.gestionecorsi.businesscomponent;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+import com.bari.gestionecorsi.architecture.dao.CorsistaDAO;
+import com.bari.gestionecorsi.architecture.dao.DAOException;
+import com.bari.gestionecorsi.architecture.dbaccess.DBAccess;
+import com.bari.gestionecorsi.businesscomponent.model.Corsista;
+
+
+
+
+public class CorsistaBC {
+	private Connection conn;
+	
+	public CorsistaBC() throws ClassNotFoundException, DAOException, IOException {
+		conn = DBAccess.getConnection();
+	}
+
+	public void createOrUpdate(Corsista corsista) throws ClassNotFoundException, IOException, DAOException {
+		if (corsista.getIdCorsista() > 0)
+			CorsistaDAO.getFactory().update(conn, corsista);
+		else {
+			corsista.setIdCorsista(CorsistaIdGenerator.getInstance().getNextId());
+			CorsistaDAO.getFactory().create(conn, corsista);
+		}
+	}
+	
+	public void delete(Corsista corsista) throws DAOException {
+		CorsistaDAO.getFactory().delete(conn, corsista);
+	}
+	
+	public Corsista[] getAll() throws DAOException {
+		return CorsistaDAO.getFactory().getAll(conn);
+	}
+}
