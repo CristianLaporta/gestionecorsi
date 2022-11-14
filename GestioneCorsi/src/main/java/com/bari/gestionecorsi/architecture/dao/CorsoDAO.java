602bb5967c21d78aa0fd0d21154285bf8a9bddc5
VitoDebellis <vito.debellis.1993@gmail.com>
2022-11-14 17:03:02 +0100

    Aggiunte DAOConstants

---
+++ GestioneCorsi/src/main/java/com/bari/gestionecorsi/architecture/dao/CorsoDAO.java
@@ -0,0 +1,5 @@
+package com.bari.gestionecorsi.architecture.dao;
+
+public class CorsoDAO {
+
+}
---
+++ GestioneCorsi/src/main/java/com/bari/gestionecorsi/architecture/dao/DAOConstants.java
@@ -0,0 +1,32 @@
+package com.bari.gestionecorsi.architecture.dao;
+
+public interface DAOConstants {
+	String SELECT_ADMIN_PASS = "select password_admin from admin where username_admin= ?";
+
+	// -----------CORSISTA--------------
+	String SELECT_CORSISTA = "select * from corsista";
+	String UPDATE_CORSISTA = "update corsista set nome_corsista = ? , cognome_corsista = ? , precedenti_formativi_corsista = ? where id_corsista=?";
+	String DELETE_CORSISTA = "delete from corsista where id_corsista=?";
+	String SELECT_CORSISTA_BYID = "select * from corsista where id_corsista = ?";
+
+	// -----------DOCENTE--------------
+	String SELECT_DOCENTE = "select * from docente";
+	String UPDATE_DOCENTE = "update docente set nome_docente = ? , cognome_docente = ? , cv_docente = ? where id_docente=?";
+	String DELETE_DOCENTE = "delete from corsista where id_corsista=?";
+	String SELECT_DOCENTE_BYID = "select * from docente where id_docente = ?";
+
+	
+	// -----------CORSO--------------
+	String SELECT_CORSO = "select * from corso";
+	String UPDATE_CORSO = "update corso set nome_corso = ? , data_fine_corso = ? , costo_corso = ? , commenti_corso = ?, aula_corso = ?, id_docente = ? where id_corso=?";
+	String DELETE_CORSO = "delete from corso where id_corso=?";
+	String SELECT_CORSO_BYID = "select * from corso where id_corso = ?";
+	
+	//------CORSISTACORSO---------
+	String SELECT_CORSISTACORSO= "select * from corsista_corso";
+	
+	//----------SEQUENZE----------
+	String SELECT_CORSISTASEQ = "select corsista_seq.nextval from dual"; // per recuperare id sequenza
+	String SELECT_CORSOSEQ = "select corso_seq.nextval from dual";
+
+}
