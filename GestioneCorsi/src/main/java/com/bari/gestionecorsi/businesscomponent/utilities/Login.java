d7a2560620877a3de9117934a531ba952343cbab
Angelo <Angelo@192.168.50.4>
2022-11-15 14:25:18 +0100

    Commit del Login (Lato Backend)

--- GestioneCorsi/src/main/java/com/bari/gestionecorsi/businesscomponent/utilities/Login.java
+++ GestioneCorsi/src/main/java/com/bari/gestionecorsi/businesscomponent/utilities/Login.java
@@ -1,5 +1,32 @@
 package com.bari.gestionecorsi.businesscomponent.utilities;
 
-public class Login {
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import com.bari.gestionecorsi.architecture.dao.DAOConstants;
+import com.bari.gestionecorsi.architecture.dao.DAOException;
+import com.bari.gestionecorsi.architecture.dbaccess.DBAccess;
+
+public class Login implements DAOConstants {
+	private Connection conn;
+	
+	public Login() throws ClassNotFoundException, DAOException, IOException {
+		conn = DBAccess.getConnection();
+	}
+	
+	public String getAdminPass(String username) throws DAOException {
+		try {
+			PreparedStatement ps = conn.prepareStatement(SELECT_ADMIN_PASS);
+			ps.setString(1, username);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next())
+				return rs.getString(1);
+			return null;
+		} catch (SQLException sql) {
+			throw new DAOException(sql);
+		}
+	}
 }
