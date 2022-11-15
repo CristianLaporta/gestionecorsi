6d2271059eeb978309aff0e2ba7eef26965326a1
grecolorenzo <118170333+grecolorenzo@users.noreply.github.com>
2022-11-15 11:39:52 +0100

    Sistemato CorsistaCorsoBC

--- GestioneCorsi/src/main/java/com/bari/gestionecorsi/businesscomponent/CorsistaCorsoBC.java
+++ GestioneCorsi/src/main/java/com/bari/gestionecorsi/businesscomponent/CorsistaCorsoBC.java
@@ -3,13 +3,12 @@ package com.bari.gestionecorsi.businesscomponent;
 import java.io.IOException;
 import java.sql.Connection;
 
-import com.bari.gestionecorsi.architecture.dao.CorsistaDAO;
+import com.bari.gestionecorsi.architecture.dao.CorsistaCorsoDAO;
 import com.bari.gestionecorsi.architecture.dao.DAOException;
 import com.bari.gestionecorsi.architecture.dbaccess.DBAccess;
-import com.bari.gestionecorsi.businesscomponent.model.Corsista;
+import com.bari.gestionecorsi.businesscomponent.model.CorsistaCorso;
+
 public class CorsistaCorsoBC {
-	
-		
 		private Connection conn;
 		
 		public CorsistaCorsoBC() throws ClassNotFoundException, DAOException, IOException {
@@ -17,20 +16,10 @@ public class CorsistaCorsoBC {
 		}
 		
 		
-		public void Update(Corsista corsista) throws DAOException, ClassNotFoundException, IOException {	
-			CorsistaDAO.getFactory().update(conn, corsista); 
+		public void create(CorsistaCorso corsistaCorso) throws DAOException, ClassNotFoundException, IOException {	
+			CorsistaCorsoDAO.getFactory().create(conn, corsistaCorso); 
 		}
 
-		public Corsista[] getArticoli() throws DAOException {
-			return CorsistaDAO.getFactory().getAll(conn);
-		}
-		
-		public void delete(Corsista corsista) throws DAOException {
-			CorsistaDAO.getFactory().delete(conn, corsista);
-		}
-
-		public Corsista findById (long id) throws DAOException {
-			return CorsistaDAO.getFactory().getById(conn, id);
-		}
+	
 
 }
