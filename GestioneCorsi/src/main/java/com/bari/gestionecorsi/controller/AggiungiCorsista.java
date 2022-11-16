b6a1c806f8edea170874290b1b8177356e6f5114
VitoDebellis <vito.debellis.1993@gmail.com>
2022-11-16 14:57:11 +0100

    commit aggiungiCorsista

--- GestioneCorsi/src/main/java/com/bari/gestionecorsi/controller/AggiungiCorsista.java
+++ GestioneCorsi/src/main/java/com/bari/gestionecorsi/controller/AggiungiCorsista.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.bari.gestionecorsi.businesscomponent.facade.AdminFacade;
 import com.bari.gestionecorsi.businesscomponent.model.Corsista;
 
 /**
@@ -18,6 +19,16 @@ public class AggiungiCorsista extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		Corsista corsista = getCorsista(request);
+		if (corsista != null) {
+			try {
+				AdminFacade.getInstance().createOrUpdate(corsista);
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new ServletException(e.getMessage());
+			}
+			response.sendRedirect("admin.jsp");
+		}
 		
 	}
 	
@@ -27,12 +38,19 @@ public class AggiungiCorsista extends HttpServlet {
 			long id = Long.valueOf(request.getParameter("id"));
 			String nome = request.getParameter("nome_corsista");
 			String cognome = request.getParameter("cognome_corsista");
-			
+			String precedenti = request.getParameter("precedenti");
+			corsista = new Corsista();
+			corsista.setIdCorsista(id);
+			corsista.setNomeCorsista(nome);
+			corsista.setCognomeCorsista(cognome);
+			corsista.setPrecedentiCorsista(precedenti);
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		}
 		
 		return corsista;
 	}
+	
+	
 
 }
