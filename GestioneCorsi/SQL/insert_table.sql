c6f3e1d8d8556e837f72f08c265fa706396fab4b
Angelo <Angelo@192.168.50.4>
2022-11-17 12:58:39 +0100

    modifica insert table

--- GestioneCorsi/SQL/insert_table.sql
+++ GestioneCorsi/SQL/insert_table.sql
@@ -7,4 +7,7 @@ insert into docente values(4, 'Salvatore', 'Carlucci', 'Un tizio qualsiasi');
 
 insert into corso values(1, 'Java', '10-MAR-2021', '11-GIU-2021', 650, 'Corso di Java', '2A', 12, 4);
 insert into corso values(2, 'PHP', '10-OTT-2020', '05-FEB-2021', 50, 'corso php', '1A', 12, 1);
+insert into corso values(3, 'Angular', '12-OTT-2022', '05-FEB-2023', 50, 'corso Angular', '4A', 12, 3);
+insert into corso values(4, 'Flutter', '10-SET-2022', '12-MAR-2023', 50, 'corso Flutter', '5A', 12, 2);
+
 commit
